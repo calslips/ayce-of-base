0b87983e5fb8721ee9010534d3f6a9e8ab39a37f
Caleb Phillips <csphillips5@gmail.com>
2022-11-12 15:53:01 -0500

    implement bcrypt to encrypt pw for db storage

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -14,12 +15,31 @@ const userSchema = new mongoose.Schema({
     unique: true,
     max: 40,
   },
-  passowrd: {
+  password: {
     type: String,
     require: true,
     min: 6,
   },
 }, { timestamps: true });
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
